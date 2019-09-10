6b8547fb1f5a8c41ef5ca38350032cc410258223
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2019-09-10 14:35:34 +0200

    fandt en navigations bar på nettet :i tjek GodStyle.css og _Layout

--- TheLeftOvers/Startup.cs
+++ TheLeftOvers/Startup.cs
@@ -21,12 +21,10 @@ namespace TheLeftOvers
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -35,7 +33,6 @@ namespace TheLeftOvers
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -45,7 +42,6 @@ namespace TheLeftOvers
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
